b48cd9e58183dad9b9183dffd582ec1b76a47261
Peter Briggs <146312751+pwbriggs@users.noreply.github.com>
2024-07-08 21:45:01 -0700

    feat: initial migration to Vite
    Go through Remix's migration guide from Classic Compiler to Vite. This doesn't work yet (hydration errors abound), but it's a good start.

--- app/root.tsx
+++ app/root.tsx
@@ -1,18 +1,11 @@
 import { Box, Typography } from "@mui/joy";
-import { cssBundleHref } from "@remix-run/css-bundle";
-import type { LinksFunction } from "@remix-run/node";
 import {
     Links,
-    LiveReload,
     Meta,
     Outlet,
     Scripts,
     ScrollRestoration,
 } from "@remix-run/react";
-
-export const links: LinksFunction = () => [
-    ...(cssBundleHref ? [{ rel: "stylesheet", href: cssBundleHref }] : []),
-];
 
 export default function App() {
     return (
@@ -42,7 +35,6 @@ export default function App() {
                 <Outlet />
                 <ScrollRestoration />
                 <Scripts />
-                <LiveReload />
             </body>
         </html>
     );
