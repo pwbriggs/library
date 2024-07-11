8bc414383fd8aa89861ecb37b0d234722186eb21
Peter Briggs <146312751+pwbriggs@users.noreply.github.com>
2024-07-11 03:25:33 +0000

    feat: remove construction warning header (#151)
    Get rid of the site-wide "under construction" notice. It's unhelpful.

--- app/root.tsx
+++ app/root.tsx
@@ -6,7 +6,7 @@ import {
     ScrollRestoration,
 } from "@remix-run/react";
 
-import { ColorSchemeScript, MantineProvider, Box } from '@mantine/core';
+import { ColorSchemeScript, MantineProvider } from '@mantine/core';
 import '@mantine/core/styles.css';
 
 export default function App() {
@@ -21,12 +21,6 @@ export default function App() {
             </head>
             <body style={{ margin: 0, padding: 0, overflowWrap: "break-word" }}>
                 <MantineProvider>
-                    <Box bg="yellow.2" p="sm" ta="center">
-                        <b>
-                            ⚠️ Site under construction 🏗️
-                        </b> We&apos;re still in the initial early development stage. All pages on this
-                        site are <strong>tests only.</strong>
-                    </Box>
                     <Outlet />
                 </MantineProvider>
                 <ScrollRestoration />
