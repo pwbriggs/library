db1e805886769bbe8e617085effae5903ca7f194
Peter Briggs <146312751+pwbriggs@users.noreply.github.com>
2024-07-10 18:05:45 -0700

    feat: remove construction warning header
    Get rid of the site-wide "under construction" notice. It's unhelpful.

--- app/root.tsx
+++ app/root.tsx
@@ -21,12 +21,6 @@ export default function App() {
             </head>
             <body style={{ margin: 0, padding: 0, overflowWrap: "break-word" }}>
                 <MantineProvider>
-                    <Box bg="yellow.2" p="sm" ta="center">
-                        <b>
-                            ⚠️ Site under construction 🏗️
-                        </b> We&apos;re still in the initial early development stage. All pages on this
-                        site are <strong>tests only.</strong>
-                    </Box>
                     <Outlet />
                 </MantineProvider>
                 <ScrollRestoration />
