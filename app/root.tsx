ecca7b54a3ab90aabb5494a44d32632d78bb47af
Peter Briggs <146312751+pwbriggs@users.noreply.github.com>
2024-09-22 22:51:52 -0700

    ci: switch to biome from eslint
    Finally give up on ESLint for Biome.js. Run Biome fixes, and swap out ESLint for Biome in CI workflow.

--- app/root.tsx
+++ app/root.tsx
@@ -6,15 +6,18 @@ import {
     ScrollRestoration,
 } from "@remix-run/react";
 
-import { ColorSchemeScript, MantineProvider } from '@mantine/core';
-import '@mantine/core/styles.css';
+import { ColorSchemeScript, MantineProvider } from "@mantine/core";
+import "@mantine/core/styles.css";
 
 export default function App() {
     return (
         <html lang="en">
             <head>
                 <meta charSet="utf-8" />
-                <meta name="viewport" content="width=device-width, initial-scale=1" />
+                <meta
+                    name="viewport"
+                    content="width=device-width, initial-scale=1"
+                />
                 <Meta />
                 <Links />
                 <ColorSchemeScript />
--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -2,9 +2,7 @@ import { Box, Center, Text } from "@mantine/core";
 import type { MetaFunction } from "@remix-run/node";
 
 export const meta: MetaFunction = () => {
-    return [
-        { title: "Under Construction" }
-    ];
+    return [{ title: "Under Construction" }];
 };
 
 export default function Index() {
@@ -13,7 +11,8 @@ export default function Index() {
             <Box color="warning" bg="red.2" ta="center" my="sm" p="lg">
                 <Text fw="bold">🚧 Site under construction 🏗️</Text>
                 <Text>
-                    We&apos;re still developing this app! Check back soon for more updates.
+                    We&apos;re still developing this app! Check back soon for
+                    more updates.
                 </Text>
             </Box>
         </Center>
