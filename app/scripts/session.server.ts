c8e40330cbff6c5bc29e7cdc8f35c019c51212a9
Peter Briggs <146312751+pwbriggs@users.noreply.github.com>
2024-09-23 18:48:37 -0700

    ramp up security for cookies

--- app/scripts/session.server.ts
+++ app/scripts/session.server.ts
@@ -1,14 +1,29 @@
 import { createCookieSessionStorage } from "@remix-run/node";
+import cryptoRandomString from "crypto-random-string";
 
 // TODO replace this with better session storage
+
+let cookieSecret = process.env.COOKIE_SECRET;
+
+if (!cookieSecret) {
+    if (process.env.NODE_ENV === "production") {
+        console.error(
+            "Please specify a COOKIE_SECRET in production. For now, we have set a random one: this \
+            breaks all existing sessions and will continue doing so until you set a COOKIE_SECRET.",
+        );
+        cookieSecret = cryptoRandomString({ length: 32, type: "base64" });
+    } else {
+        cookieSecret = "d3v3l0p";
+    }
+}
 
 export const sessionStorage = createCookieSessionStorage({
     cookie: {
         name: "library_session", // use any name you want here
         sameSite: "strict",
         path: "/",
-        //***httpOnly: true,
-        secrets: ["s3cr3t"], // replace this with an actual secret
+        httpOnly: true,
+        secrets: [cookieSecret],
         secure: process.env.NODE_ENV === "production", // enable this in prod only
     },
 });
