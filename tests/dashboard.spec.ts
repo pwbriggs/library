b0349a90c39c906564e17fd28c62b3e804068e1d
Peter Briggs <146312751+pwbriggs@users.noreply.github.com>
2024-09-23 18:11:23 -0700

    build: remove Playwright for now (#179)
    Playwright currently adds complication, clutter, and slow CI to the project. Once we've build something substantial, we can reenable it.

--- tests/dashboard.spec.ts
+++
@@ -1,8 +0,0 @@
-import { expect, test } from "@playwright/test";
-
-test("has title", async ({ page }) => {
-    await page.goto("/");
-    await expect(
-        page.getByText("Site under construction").first(),
-    ).toBeVisible();
-});
