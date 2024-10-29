4d4dc66766fb986b785146d4a1a2d1f36b05d3aa
Peter Briggs <146312751+pwbriggs@users.noreply.github.com>
2024-10-28 23:09:00 -0700

    feat: add basic staff homepage and header
    Make a simple wrapper for `/staff/*` paths that adds a header to the page. Also write a small staff index page with links to staff tools.

---
+++ app/routes/staff._index.tsx
@@ -0,0 +1,18 @@
+import { List, Text, Title } from "@mantine/core";
+import { Link } from "@remix-run/react";
+import { IconSparkles } from "@tabler/icons-react";
+
+export default function StaffHome() {
+    return (
+        <>
+            <Title>Staff Home</Title>
+            <Text>
+                Welcome, wonderful librarians! Here's some staff tools you can
+                use:
+            </Text>
+            <List icon={<IconSparkles />}>
+                <List.Item><Link to="/staff/create-user">Create new user</Link></List.Item>
+            </List>
+        </>
+    );
+}
