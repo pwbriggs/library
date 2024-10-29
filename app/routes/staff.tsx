44d9c3722c72b44fa59f6261524bb6865a7f3fbe
Peter Briggs <146312751+pwbriggs@users.noreply.github.com>
2024-10-28 23:15:09 -0700

    feat: add basic staff homepage and header (#190)
    Make a simple wrapper for `/staff/*` paths that adds a header to the page. Also write a small staff index page with links to staff tools.

--- app/routes/staff.tsx
+++ app/routes/staff.tsx
@@ -1,6 +1,20 @@
 import type { LoaderFunctionArgs } from "@remix-run/node";
+import { Link, Outlet } from "@remix-run/react";
 import { auth } from "~/scripts/auth.server";
 import { trimUrl } from "~/scripts/urls.server";
+
+export default function staffHeader() {
+    return (
+        <>
+            <header style={{ backgroundColor: "green", color: "white" }}>
+                <Link to="/staff" style={{ color: "white" }}>
+                    Staff home
+                </Link>
+            </header>
+            <Outlet />
+        </>
+    );
+}
 
 export async function loader({ request }: LoaderFunctionArgs) {
     const user = await auth.isAuthenticated(request, {
