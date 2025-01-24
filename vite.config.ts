0dc02fee8aa21a3d9e89d425460e2a67e794b74c
Peter Briggs <146312751+pwbriggs@users.noreply.github.com>
2025-01-23 22:25:47 -0800

    switch vite config to ts and enable singleFetch flag

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,12 @@ import { vitePlugin as remix } from "@remix-run/dev";
 import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
 
+declare module "@remix-run/node" {
+    interface Future {
+        v3_singleFetch: true;
+    }
+}
+
 export default defineConfig({
     plugins: [
         remix({
@@ -10,6 +16,7 @@ export default defineConfig({
                 v3_relativeSplatPath: true,
                 v3_throwAbortReason: true,
                 v3_lazyRouteDiscovery: true,
+                v3_singleFetch: true,
             },
         }),
         tsconfigPaths(),
